f7545cbbf3a0cbcc2c965db58b87b4c876436dd4
CasperFBakker <casper.f.bakker@gmail.com>
2022-11-07 09:15:38 +0100

    update

--- Config_Folder.py
+++ Config_Folder.py
@@ -1,14 +1,6 @@
 
 Date = '26_10_22'
 
-# def Setup_Dir(Date):
-#     Photo_Dir = '/home/casper/Documents/Aardwetenschappen/MSc Thesis/Photo/' + Date + '/'
-#     ImageData_Dir = '/home/casper/Documents/Python/pyDGS GUI/Output data/Image_data/' + Date + '/'
-#     OutputData_Dir = '/home/casper/Documents/Python/pyDGS GUI/Output data/' + Date + '/'
-#     OutputCorrected_Dir = '/home/casper/Documents/Python/pyDGS GUI/Output data/' + Date + '/Corrected/'
-#     OutputOriginal_Dir = '/home/casper/Documents/Python/pyDGS GUI/Output data/' + Date + '/UnCorrected/'
-#     return Photo_Dir, ImageData_Dir, OutputData_Dir, OutputCorrected_Dir, OutputOriginal_Dir
-
 def Setup_Dir(Date, SubFolder=False):
     if SubFolder:        
         Photo_Dir = '/home/casper/Documents/Aardwetenschappen/MSc Thesis/Photo/' + Date + '/' + SubFolder + '/'
